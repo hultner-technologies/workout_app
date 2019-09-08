d3cde80cc197fb1f7b756244d24dc28339678f14
Alexander Hultnér <ahultner@gmail.com>
2019-09-08 23:58:45 +0200

    Added some more plans to db script

--- workout_app/scripts/init_db.py
+++ workout_app/scripts/init_db.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 import click
-import dotenv
+
+# import dotenv
+
 
 @click.command()
-@click.option('--database' help='Specify a specific database name')
+@click.option("--database", help="Specify a specific database name")
 def setup_db(database):
+    # Get connection string from .env/user input
+    # - (See if database exists, if not create)
+    # Fetch content of all psql/sql files in database directory
+    # Execute in correct order to database
+    # Seed with default plans?
     pass
 
 
+if __name__ == "__main__":
+    # Pass args?
+    setup_db()
