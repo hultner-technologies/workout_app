365d7b2a8c889de2b1a9bbe337a211b9abbb25ff
Alexander Hultnér <ahultner@gmail.com>
2022-07-16 18:45:04 +0200

    feat: Add more data to exercise view.

--- database/110_views_full_exercise.sql
+++ database/110_views_full_exercise.sql
@@ -2,14 +2,17 @@ create view full_exercise as
 (
     select exercise_id
      , be.base_exercise_id
-     , name
-     , description
+     , ex.session_schedule_id
+     , ss.name as session
+     , be.name
+     , be.description
      , reps
      , sets
      , rest
-     , links
-     , exercise.data as data
+     , be.links
+     , ex.data as data
      , be.data       as base_data
-    from exercise
-         join base_exercise be on be.base_exercise_id = exercise.base_exercise_id
+    from exercise ex
+         join base_exercise be on be.base_exercise_id = ex.base_exercise_id
+         join session_schedule ss on ex.session_schedule_id = ss.session_schedule_id
 )
