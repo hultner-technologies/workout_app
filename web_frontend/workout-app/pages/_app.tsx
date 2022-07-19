bae502374001e2722717f57775176e55ddcb8d47
Alexander Hultnér <ahultner@gmail.com>
2022-07-19 23:30:24 +0200

    chore: Setup API-client base route

--- web_frontend/workout-app/pages/_app.tsx
+++ web_frontend/workout-app/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import { OpenAPI } from "../src/client";
 import Head from "next/head";
 
+OpenAPI.BASE = "http://192.168.1.98:8001";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
