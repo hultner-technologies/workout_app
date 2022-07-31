7585e819452652c5cf045078736e9e2ab98705a9
Alexander Hultnér <ahultner@gmail.com>
2022-07-31 16:21:01 +0200

    feat: Make it possible to draft new session without creating.

---
+++ database/210_draft_session_exercises.sql
@@ -0,0 +1,47 @@
+create or replace function draft_session_exercises(performed_session_id_ uuid)
+    returns table (exercise_id uuid
+                  , performed_session_id uuid
+                  , name text, reps positive_int[]
+                  , rest interval[]
+                  , weight positive_int
+                  )
+AS $$
+with performed_exercise_base as (
+    select pe.*
+         , base_exercise_id
+         , app_user_id
+         , (select min(r) from unnest(pe.reps) r)
+               >= s.progression_limit * e.reps
+             as successful
+    from performed_exercise pe
+      join exercise e on pe.exercise_id = e.exercise_id
+      join performed_session p on pe.performed_session_id = p.performed_session_id
+      join session_schedule s on e.session_schedule_id = s.session_schedule_id
+)
+select
+       distinct  on (fe.exercise_id) fe.exercise_id,
+       ps.performed_session_id,
+       fe.name                         as name,
+       array_fill(fe.reps, array[fe.sets]) as reps,
+       array_fill(fe.rest, array[fe.sets]) as rest,
+       coalesce(max(pe.weight), 0)
+           + (case when pe.successful then fe.step_increment else 0 end)
+           as weight
+from performed_session ps
+         join full_exercise fe on ps.session_schedule_id = fe.session_schedule_id
+         left join performed_exercise_base pe
+                   on fe.base_exercise_id = pe.base_exercise_id
+                          and pe.app_user_id = ps.app_user_id
+where ps.performed_session_id = performed_session_id_
+group by fe.base_exercise_id
+       , fe.name
+       , fe.exercise_id
+       , ps.performed_session_id
+       , fe.reps
+       , fe.sets
+       , fe.rest
+       , fe.step_increment
+       , pe.successful
+order by fe.exercise_id;
+$$
+LANGUAGE SQL;
