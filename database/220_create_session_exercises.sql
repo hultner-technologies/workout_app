1d6d964d46c328c50eba36195a4a6caac5407f3d
Alexander Hultnér <ahultner@gmail.com>
2022-07-31 15:40:47 +0200

    feat: Use step_increment in session creation.

--- database/220_create_session_exercises.sql
+++ database/220_create_session_exercises.sql
@@ -1,4 +1,4 @@
-create or replace function create_session_exercises(performed_session_id_ uuid) returns table (like performed_exercise)
+create or replace function create_session_exercises(performed_session_id_ uuid) returns table ("like" performed_exercise)
 AS $$
 insert into performed_exercise (exercise_id, performed_session_id, name, reps, rest, weight )
 with performed_exercise_base as (
@@ -15,13 +15,13 @@ select -- fe.base_exercise_id,
        fe.name                         as name,
        array_fill(fe.reps, array[fe.sets]) as reps,
        array_fill(fe.rest, array[fe.sets]) as rest,
-       coalesce(max(pe.weight), 0) + (case when pe.successful then 2500 else 0 end) as weight
+       coalesce(max(pe.weight), 0) + (case when pe.successful then fe.step_increment else 0 end) as weight
 from performed_session ps
          join full_exercise fe on ps.session_schedule_id = fe.session_schedule_id
          left join performed_exercise_base pe
                    on fe.base_exercise_id = pe.base_exercise_id and pe.app_user_id = ps.app_user_id --and pe.successful is true
 where ps.performed_session_id = performed_session_id_
-group by fe.base_exercise_id, fe.name, fe.exercise_id, ps.performed_session_id, fe.reps, fe.sets, fe.rest, pe.successful
+group by fe.base_exercise_id, fe.name, fe.exercise_id, ps.performed_session_id, fe.reps, fe.sets, fe.rest, fe.step_increment, pe.successful
 order by fe.exercise_id
 returning *;
 $$
