f49ddca7d9a1b5e8987d70353574a5cf6d5d72a5
Alexander Hultnér <ahultner@gmail.com>
2024-09-26 14:28:50 +0200

    feat: Update model for performed exercise.

--- database/070_PerformedExercise.sql
+++ database/070_PerformedExercise.sql
@@ -4,8 +4,8 @@ CREATE TABLE performed_exercise (
     , exercise_id uuid REFERENCES exercise(exercise_id)
     , performed_session_id uuid REFERENCES performed_session(performed_session_id) NOT NULL
     , name text
-    -- Sets is a list of reps e.g. [ 10, 10, 9, 8, 7, ], sets is the lenght
-    , reps positive_int[] NOT NULL
+    -- Sets is a list of reps e.g. [ 10, 10, 9, 8, 7, ], sets is the length
+    , reps positive_int[] default ARRAY [ 10, 10, 10, 10, 10 ] NOT NULL
     , sets positive_int GENERATED ALWAYS AS (array_length(reps, 1)) STORED
     -- The generated column above is a PG12 feature, if you use PG11 comment
     -- it out and use the row below for now.
@@ -13,15 +13,15 @@ CREATE TABLE performed_exercise (
     -- TODO: See if I can set default via session_schedule_id foreign key,
     -- for both rest and reps
     , rest interval[] DEFAULT ARRAY[
-        interval '00:01:00'
-        , interval '00:01:00'
-        , interval '00:01:00'
-        , interval '00:01:00'
-        , interval '00:01:00'
-        ]
+        interval '00:02:00'
+        , interval '00:02:00'
+        , interval '00:02:00'
+        , interval '00:02:00'
+        , interval '00:02:00'
+        ] NOT NULL
     , weight positive_int
     , started_at timestamp default now()
-    , completed_at timestamp default now()
+    , completed_at timestamp default null
     , note text
     , data jsonb
 );
