203fba1f8a3f53763f3f25bb5e5e942b382a22e1
Alexander Hultnér <ahultner@gmail.com>
2024-09-26 14:28:25 +0200

    feat: Add special set.

---
+++ database/071_SpecialSet.sql
@@ -0,0 +1,55 @@
+-- Special sets are a special type of set for a performed_exercise.
+-- These include warm-up, drop sets, super sets, myo reps, pyramid sets, AMRAP,
+-- and so on.
+-- Drop-sets are a set where you decrease the weight after each set.
+-- Super-sets are a set where you do two exercises back to back.
+-- Myo-reps are a set where you do a set to failure, rest for a short period,
+-- then do another set to failure, and so on.
+-- Pyramid sets are a set where you increase the weight after each set.
+-- AMRAP is as many reps as possible.
+-- The special_set table is a many-to-many relationship between performed_exercise
+-- and special_set_type.
+-- The special_set_type table is a list of special set types.
+-- The special_set table has a weight column to allow for drop sets and pyramid
+-- sets.
+-- The special_set table has a reps column to allow for AMRAP.
+-- The special_set table has a rest column to allow for myo-reps.
+-- The special_set table has a order column to allow for pyramid sets.
+-- The special_set table has a note column to allow for notes.
+-- The special_set table has a data column to allow for future expansion.
+-- The special_set table has a started_at column to allow for auditing.
+-- The special_set table has a completed_at column to allow for auditing.
+-- The special_set table has a parent_special_set_id column to allow for super sets
+-- and myo-reps to be nested.
+-- The special_set table has a performed_exercise_id column to allow for the set to
+-- connect to a performed_exercise.
+
+create table special_set_type (
+    name text PRIMARY KEY CHECK (name <> '')
+    , description text
+);
+insert into special_set_type (name, description)
+values
+    ('warm-up', 'A warm-up set.')
+    , ('drop-set', 'A set where you decrease the weight after each set.')
+    , ('super-set', 'A set where you do two exercises back to back.')
+    , ('myo-rep', 'A set where you do a set to failure, rest for a short period, then do another set to failure, and so on.')
+    , ('pyramid-set', 'A set where you increase the weight after each set.')
+    , ('AMRAP', 'As many reps as possible.')
+    , ('other', 'A set that does not fit into the other categories.');
+
+
+CREATE TABLE special_set (
+    special_set_id uuid DEFAULT uuid_generate_v1mc() PRIMARY KEY
+    , performed_exercise_id uuid REFERENCES performed_exercise(performed_exercise_id) NOT NULL
+    , sepcial_set_type text REFERENCES special_set_type(name) NOT NULL
+    , weight positive_int NOT NULL
+    , reps positive_int NOT NULL
+    , rest interval NOT NULL default interval '00:01:00'
+    , "order" positive_int NOT NULL
+    , note text
+    , data jsonb
+    , started_at timestamp default now()
+    , completed_at timestamp default null
+    , parent_special_set_id uuid REFERENCES special_set(special_set_id)
+);
